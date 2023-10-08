767117c46c8cac86f65d8999d44bd17eca5796b8
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-10-08 10:29:46 +0530

    made navbar sticky

--- client/src/app/upload/page.tsx
+++ client/src/app/upload/page.tsx
@@ -1,11 +1,23 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import React from "react";
+import axios from "axios"
 
 type Props = {};
 
 export default function UploadImage({}: Props) {
+  const uploadImage=(e:React.ChangeEvent<HTMLInputElement>)=>{
+    console.log(e.target.files[0])
+    const formdata = new FormData();
+    formdata.append("image", e.target.files[0])
+    axios.post("https://divyanshurana312.pythonanywhere.com/predictions/predict/",formdata)
+    .then((res)=>{
+      console.log(res)
+    })
+  }
+
   return (
     <div className=" h-full overflow-auto">
       <div className="border-dashed border-2 p-12 flex flex-col justify-center items-center gap-4">
@@ -15,8 +27,7 @@ export default function UploadImage({}: Props) {
           editor. Upload, edit, and share instantly from one place.
         </p>
         <div className="grid w-full max-w-sm items-center gap-1.5 relative">
-          <Input id="file" type="file" className="text-foregroundTemp hidden" />
-
+          <input id="file" type="file" className="text-foregroundTemp hidden" onChange={uploadImage}/>
           <Label
             className="bg-backgroundTemp text-foregroundTemp p-4 text-center cursor-pointer"
             htmlFor="file"
